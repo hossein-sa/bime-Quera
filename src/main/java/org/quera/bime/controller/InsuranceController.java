a5ec8e56d349cdd2c17e395c953df55b48a5c8d1
Hossein Sadeghi <sadeghi.ho@hotmail.com>
2023-03-13 15:50:28 +0330

    refactore

--- src/main/java/org/quera/bime/controller/InsuranceController.java
+++ src/main/java/org/quera/bime/controller/InsuranceController.java
@@ -1,10 +1,6 @@
 package org.quera.bime.controller;
 
-import org.quera.bime.model.dto.CreateInsuranceDto;
 import org.quera.bime.service.InsuranceService;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,14 +11,5 @@ public class InsuranceController {
 
     public InsuranceController(InsuranceService insuranceService) {
         this.insuranceService = insuranceService;
-    }
-    @PostMapping("/save")
-    public ResponseEntity<String> createInsurance(@RequestBody CreateInsuranceDto createInsuranceDto) {
-        try {
-            insuranceService.createInsurance(createInsuranceDto);
-            return ResponseEntity.ok("Insurance saved");
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body("Insurance saving problem!");
-        }
     }
 }
